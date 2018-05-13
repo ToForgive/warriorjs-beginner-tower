078daab69eef9b505f9bf73a0682a52658389d5a
Shiung <hh.shiung@gmail.com>
2018-05-13 20:34:08 +0800

    Complete Level 8

--- Player.js
+++ Player.js
@@ -12,50 +12,56 @@ class Player {
     return Boolean(warrior.health() < 20)
   }
 
-  check(warrior, direction) {
-    if (warrior.feel(direction).isCaptive()) {
-      warrior.rescue()
-    } else if (!warrior.feel(direction).isEmpty()) {
-      warrior.attack()
-    }
+  isEnemyInSight(warrior) {
+    let view = warrior.look().find(space => !space.isEmpty())
+    return view && view.isEnemy()
   }
 
   playTurn(warrior) {
-    if (warrior.feel().isWall()) {
-      warrior.pivot()
-      return
-    }
-
-    if (this.isTakingDamage(warrior)) {
-      if (warrior.health() <= 10) {
-        if (warrior.feel('backward').isEmpty()) {
-          warrior.walk('backward')
-        } else if (warrior.feel('backward').isCaptive()) {
-          warrior.rescue('backward')
-        } else {
-          warrior.rest()
-        }
-      } else if (warrior.feel().isEmpty()) {
-        warrior.walk()
-      } else if (warrior.feel().isCaptive()) {
-        warrior.rescue()
-      } else {
-        warrior.attack()
-      }
+    if (this.isEnemyInSight(warrior)) {
+      warrior.shoot()
+    } else if (warrior.feel().isEmpty()) {
+      warrior.walk()
+    } else if (warrior.feel().isCaptive()) {
+      warrior.rescue()
     } else {
-      if (this.isInjured(warrior)) {
-        warrior.rest()
-        return
-      }
-
-      if (warrior.feel().isEmpty()) {
-        warrior.walk()
-      } else if (warrior.feel().isCaptive()) {
-        warrior.rescue()
-      } else {
-        warrior.attack()
-      }
+      warrior.attack()
     }
+    // if (warrior.feel().isWall()) {
+    //   warrior.pivot()
+    //   return
+    // }
+
+    // if (this.isTakingDamage(warrior)) {
+    //   if (warrior.health() <= 10) {
+    //     if (warrior.feel('backward').isEmpty()) {
+    //       warrior.walk('backward')
+    //     } else if (warrior.feel('backward').isCaptive()) {
+    //       warrior.rescue('backward')
+    //     } else {
+    //       warrior.rest()
+    //     }
+    //   } else if (warrior.feel().isEmpty()) {
+    //     warrior.walk()
+    //   } else if (warrior.feel().isCaptive()) {
+    //     warrior.rescue()
+    //   } else {
+    //     warrior.attack()
+    //   }
+    // } else {
+    //   if (this.isInjured(warrior)) {
+    //     warrior.rest()
+    //     return
+    //   }
+
+    //   if (warrior.feel().isEmpty()) {
+    //     warrior.walk()
+    //   } else if (warrior.feel().isCaptive()) {
+    //     warrior.rescue()
+    //   } else {
+    //     warrior.attack()
+    //   }
+    // }
     this.health = warrior.health()
   }
 }
